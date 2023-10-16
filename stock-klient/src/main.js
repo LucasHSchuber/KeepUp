18dcdf4fd1f9fa85106bec99958ccf845b8e4308
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2023-10-16 10:34:26 +0200

    before delete

--- stock-klient/src/main.js
+++ stock-klient/src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { Bar } from 'vue-chartjs'
+
 const app = createApp(App)
 
 app.use(router)
--- stock-klient/src/views/StartView.vue
+++ stock-klient/src/views/StartView.vue
@@ -17,7 +17,13 @@
 </template>
 
 <script>
+import BarChart from '../components/BarChart.vue'
 
+export default {
+    components: {
+        BarChart
+    }
+}
 
 </script>
 
